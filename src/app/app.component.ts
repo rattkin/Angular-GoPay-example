7d961ea64c349777e51d708507328f5fa6573b46
Jan Müller <muj@data-norms.cz>
2021-06-16 11:41:39 +0200

    feat: simple form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular-GoPay-example';
+    title = 'Angular-GoPay-example';
+    public model = { amount: 1 };
+
+    pay(): void {
+        console.log(this.model.amount);
+    }
+
 }
